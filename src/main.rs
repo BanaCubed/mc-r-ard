9ddda8851b92d487bf691f25ecd4dfaf1e08f9e6
Rin <162780442+BanaCubed@users.noreply.github.com>
2026-06-15 09:45:02 +1200

    Diagram Finished (for now)

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,22 @@
 #![no_std]
 #![no_main]
 
+use arduino_hal::prelude::*;
 use panic_halt as _;
+
+use arduino_hal::adc;
 
 #[arduino_hal::entry]
 fn main() -> ! {
-    let dp = arduino_hal::Peripherals::take().unwrap();
+    let dp: arduino_hal::Peripherals = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
+    let mut adc = arduino_hal::Adc::new(dp.ADC, Default::default());
 
-    let mut led = pins.d13.into_output();
+    let mut input_pin = pins.a0.into_analog_input(&mut adc);
 
     loop {
-        led.toggle();
         arduino_hal::delay_ms(150);
     }
 }
