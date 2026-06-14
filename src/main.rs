daf879d5b2143fc629733534611432c786952c59
Rin <162780442+BanaCubed@users.noreply.github.com>
2026-06-15 10:26:13 +1200

    Add lcd-lcm1602-i2c as dependency

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
 #![no_std]
 #![no_main]
 
-use arduino_hal::prelude::*;
 use panic_halt as _;
-
-use arduino_hal::adc;
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp: arduino_hal::Peripherals = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+    let serial = arduino_hal::default_serial!(dp, pins, 57600);
 
     let mut adc = arduino_hal::Adc::new(dp.ADC, Default::default());
 
-    let mut input_pin = pins.a0.into_analog_input(&mut adc);
+    let input_pin = pins.a0.into_analog_input(&mut adc);
 
     loop {
         arduino_hal::delay_ms(150);
